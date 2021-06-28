e450f384e48989c3228a550f239ff7fadf1026fb
PeterSmith1 <1793446390@qq.com>
2021-06-28 19:59:56 +0800

    练习

---
+++ manner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+num1 = 100
+num2 = 100.0
+num3 = 10000000000
+print(num1)
+print(num2)
+print(num3)
+
+
+# 选择、循环
+a = 21
+b = 10
+c = 0
+if a == b:
+ print("a等于b")
+
+
