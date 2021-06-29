16f95ed9aa92735362a10f88cbf89ea430245572
PeterSmith1 <1793446390@qq.com>
2021-06-29 21:39:45 +0800

    练习

--- manner.py
+++ manner.py
@@ -46,5 +46,40 @@ while (i < 100):
 # python continue语句
 for letter in 'python':
  if letter == 'h':
-  continue
+  continue   #continue结束当前一次循环
  print(letter)
+# python break语句
+for letter1 in 'python':
+ if letter1 == 'h':
+  break      #break跳出当前循环
+ print(letter1)
+# pass语句
+def sample(number):
+ pass #不做任何操作，一般只做占位语句
+# 列表
+list1 = [1,2,3,4,5]
+list2 = ['python','study','java']
+print(list1[0])
+print(list2[1])
+
+# 元组  与列表的区别是，元祖的元素是无法修改的
+tup1 = (1,2,3,4,5)
+tup2 = (6,7,8,9,10)
+print(tup1)
+#虽然元祖不可以修改，但可以通过建立新元祖进行相应的连接组合
+tup3 = tup1 + tup2
+print(tup3)
+
+# 字典
+# 字典是另一种可变容器模型，且可存储任意类型对象。
+# 字典的每个键值 key=>value 对用冒号 : 分割，每个键值对之间用逗号 , 分割，整个字典包括在花括号 {} 中
+# example: d = {key1 : value1, key2 : value2 }
+
+dict = {'a':1,'b':2,'c':3,'d':4,'e':5}
+print(dict['a'])
+
+# 时间与日期
+import time
+print(time.strftime("%Y-%m-%d %H:%M:%S"),time.localtime())
+
+
